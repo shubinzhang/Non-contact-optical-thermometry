8b8de71d417cf8b8d34358014292cf16a32c6043
ShubinZhang <Alexzhang137@gmail.com>
2020-12-27 21:56:28 -0500

    Update config.py

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ setting_dict_upconversion = {
     "wl2":542,         #excitation wavelength for ASPL 
     "power1":20,       #Laser power for PL %
     "power2":100,      #Laser power for ASPL %
-    "exposure_time1": 0.3,  #exposure time of CCD camera for PL
-    "exposure_time2":0.3,   #exposure time of CCD camera for ASPL
-    "row_center":258,  #vertial position of emission spot in CCD camera iamge
+    "exposure_time1": 0.3,  #exposure time of CCD camera for PL (s)
+    "exposure_time2":0.3,   #exposure time of CCD camera for ASPL (s)
+    "row_center":258,  #vertial position of emission spot in CCD camera iamge 
     "row_height":11,   #emission spot height in CCD camera iamge
     "dspl" = True,     #show PL/ASPL spectra during measurement     
     "save" = True,     #save data
@@ -28,15 +28,13 @@ setting_dict_upconversion = {
     "chromex_port": 1  #spectrometer port number 
 
     #Measuremnt setting
-    "wlen_start":450,  #start wavelength of spectrum
-    "N_scans":8,       #number of spectra measured for averaging
-    "N_wl_change":10,  #number of PL/ASPL spectra measured each time 
-    "wl1":460,         #excitation wavelength for PL
-    "wl2":542,         #excitation wavelength for ASPL 
-    "power1":20,       #Laser power for PL %
-    "power2":100,      #Laser power for ASPL %
-    "exposure_time1": 0.3,  #exposure time of CCD camera for PL
-    "exposure_time2":0.3,   #exposure time of CCD camera for ASPL
+    "wlen_start":450,   #start wavelength of spectrum
+    "N_scans":8,        #number of spectra measured for averaging
+    "N_measurement":10, #number of temperature measurement
+    "waiting_time":15,  #waiting time between each measurement (s)
+    "wl":542,           #excitation wavelength
+    "power":20,         #Laser power %
+    "exposure_time": 0.3,  #exposure time of CCD camera (s)
     "row_center":258,  #vertial position of emission spot in CCD camera iamge
     "row_height":11,   #emission spot height in CCD camera iamge
     "dspl" = True,     #show PL/ASPL spectra during measurement     
